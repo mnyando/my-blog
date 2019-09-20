d65c6c042f86e15d5f9f9022a571de5fb4ac3c1a
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-20 12:45:07 +0300

    Defines the signup,login and logout routes.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,2 +1,19 @@
 from . import auth
 from .. import db
+from flask import render_template
+
+
+@auth.route("/signup")
+def signup():
+    return  render_template("auth/signup.html")
+
+
+@auth.route("/login")
+def Login():
+    return  render_template("Login.html") 
+
+
+@auth.route("/logout")
+def Logout():
+    return render_template("Logout.html")
+
