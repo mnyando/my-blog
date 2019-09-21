89d3c807b06e53c1cdf92e970818dc848bfd623a
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-21 22:38:46 +0300

    Defines the signup path.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,42 @@
 from . import auth
-from .. import db
-from flask import render_template
+from .. import db, Bcrypt
+from flask import Blueprint, render_template, redirect, url_for, request, flash, abort
+from werkzeug.security import generate_password_hash, check_password_hash
+from ..models import User
+from .forms import SignupForm
+from flask_login import login_user,current_user,login_required,logout_user
 
 
-@auth.route("/signup")
+
+
+
+
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return  render_template("auth/signup.html")
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = SignupForm()
+    if form.validate_on_submit():
+        # hashed_password = Bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account has been  created,you can log in.', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template("auth/signup.html",title="Register", form = form) 
+
+    return  render_template("auth/signup.html") 
+
+
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}')"
+
+
+
+
 
 
 @auth.route("/login")
@@ -16,4 +47,9 @@ def login():
 @auth.route("/logout")
 def logout():
     return render_template("auth/logout.html")
+
+
+
+
+
 
