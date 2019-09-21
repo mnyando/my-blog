4f6f3b35f86c0075d28035af6afde71d7ce4e8b2
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-21 22:37:28 +0300

    Adds the signup form logic.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, PasswordField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+class SignupForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired(),Length(min=3,max=21)])
+    email = StringField("email", validators=[Email(),DataRequired()])
+    password = PasswordField("password" ,validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password",validators=[DataRequired(),EqualTo("password")])
+    submit = SubmitField("Sign Up")
