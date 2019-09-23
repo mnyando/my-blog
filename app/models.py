6c8771c56134b8beac77a6bc0efbefc3803b3d77
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-23 17:37:04 +0300

    Adds the login_manager.

--- app/models.py
+++ app/models.py
@@ -1,10 +1,16 @@
 from . import db
+from app import login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    username = db.Column(db.String(1000))
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    username = db.Column(db.String(1000), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     
+@login_manager.user_loader
+def get_user(user_id):
+    return User.query.get(user_id)
