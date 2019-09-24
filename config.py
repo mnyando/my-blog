240bb65e6e08d0fc21aeba912e98fd694bf75ea1
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-09-24 20:16:25 +0300

    Solve errors on database.creates new database.

--- config.py
+++ config.py
@@ -8,18 +8,18 @@ class Config:
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    pass
+
 
 class TestConfig(Config):
     pass
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/tests'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/bloggy'
 
     DEBUG = True
 
 config_options ={
     "production":ProdConfig,
-    "default":DevConfig,
+    "development":DevConfig,
     "testing":TestConfig}
 
